02f89dc1342bb79472298eeeaa1db7d41be61841
hernandez1981 <kathya_lucia@hotmail.com>
2022-05-02 12:23:19 -0600

    6. Agregando nueva ruta /explorersInNode y regresando un objeto

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.get('/launchx', (req, res) => {
     res.send('Bienvenidos a launchx');
 });
 
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: "Explorer", msg: "Hello"};
+    res.send(explorer);
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
