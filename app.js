58be6f49ccdf93d3389ffd8fed077cf3bc7be814
hernandez1981 <kathya_lucia@hotmail.com>
2022-05-02 12:02:49 -0600

    5. Agregando nueva ruta /launchx

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello Katy!');
 });
 
+app.get('/launchx', (req, res) => {
+    res.send('Bienvenidos a launchx');
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
-})
+});
